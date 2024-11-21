dd94b4f8e24e7822eb8e98316a0b171f7f8987d7
FabriLluvia <fmu.elmundodelosjuegos@gmail.com>
2024-11-21 16:13:15 -0500

    Broken

--- register.js
+++ register.js
@@ -1,34 +1,53 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAYwh5vWLOkG4KAyeNxBBT0VWqHQeLPJjs",
-    authDomain: "lyrkr-users.firebaseapp.com",
-    projectId: "lyrkr-users",
-    storageBucket: "lyrkr-users.firebasestorage.app",
-    messagingSenderId: "635599211116",
-    appId: "1:635599211116:web:5adc7591d8e9821775a649",
-    measurementId: "G-422L5LMDGV"
+    apiKey: "AIzaSyAp83Tb4S9Cnrmhf1O3v99ZRWQtz6Zlkho",
+    authDomain: "fmu-login-services.firebaseapp.com",
+    projectId: "fmu-login-services",
+    storageBucket: "fmu-login-services.firebasestorage.app",
+    messagingSenderId: "53121289627",
+    appId: "1:53121289627:web:e9b9d46f18209bb628653e",
+    measurementId: "G-E0EZ03GSL7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// Inputs for Email & Password
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
 
-// Submit Button
-const submit = document.getElementById('submit');
-submit.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.alert("Hello world!");
-    alert("Hello world!");
-});
+const auth = getAuth();
+auth.languageCode = 'en';
+const provider = new GoogleAuthProvider();
+
+const googleLogin = document.getElementById("google-login-btn");
+
+googleLogin.addEventListener("click", function(){
+    
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+
+            console.log(user);
+            window.location.href = '../LoggedIn.html';
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+})
